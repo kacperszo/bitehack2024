74c8ab55aa50b129d1b9e81840e279fe18fb30da
kacperszo <kacperszot1@gmail.com>
2024-01-20 20:10:56 +0100

    rework of AreYouFromClinicForm

--- frontend/src/components/signup/AreYouFromClinicForm.js
+++ frontend/src/components/signup/AreYouFromClinicForm.js
@@ -1,26 +1,46 @@
 import {useState} from "react";
-import {Box, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography} from "@mui/material";
+import {
+    Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography, useTheme
+} from "@mui/material";
 import {Label} from "@mui/icons-material";
 
 export default function AreYouFromClinicForm({value, setValue}) {
-    return (
-        <Box sx={{
-            paddingTop: 2,
-            paddingBottom: 1,
-        }}>
-            <FormControl>
-                <Typography id="radio-select-are-you-from-clinic">Czy trafiłeś tutaj z kliniki?</Typography>
-                <RadioGroup
-                    aria-labelledby="radio-select-are-you-from-clinic"
-                    defaultValue={false}
-                    value={value}
-                    onChange={e => setValue(e.target.value=="true")}
-                    name="radio-select-are-you-from-clinic"
-                >
-                    <FormControlLabel value={true} control={<Radio/>} label="Tak"/>
-                    <FormControlLabel value={false} control={<Radio/>} label="Nie"/>
-                </RadioGroup>
-            </FormControl>
+    const theme = useTheme()
+    const sharedBtnStyle = {
+        padding: 2, marginTop: 2, width: "100%", display: "block",
+    }
+    const selectedBtnStyle = {
+        backgroundColor: theme.palette.primary.dark,
+    }
+    const notSelectedBtnStyle = {
+        color: theme.palette.primary.dark, borderColor: theme.palette.primary.dark,
+        // borderWidth: "2px" will break hover effect
+    }
+    return (<Box sx={{
+        paddingBottom: 1, width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+
+    }}>
+        <Box>
+            <Typography textAlign={"center"} sx={{display: "block", p: 3}} fontWeight={600} variant={"h4"}>Czy trafiłeś
+                tutaj z
+                kilniki?</Typography>
+            <Typography sx={{display: "block", p: 3}} textAlign={"center"} variant={"subheader"}>Ta informacja
+                usprawni
+                process
+                rejestracji</Typography>
         </Box>
-    )
+        <Box sx={{width: "100%", maxWidth: 400}}>
+            <Button fullWidth variant={value ? "contained" : "outlined"}
+                    onClick={() => setValue(true)}
+                    sx={value ? ({...sharedBtnStyle, ...selectedBtnStyle}) : ({...sharedBtnStyle, ...notSelectedBtnStyle})}
+            >Tak</Button>
+            <Button fullWidth variant={(!value) ? "contained" : "outlined"}
+                    onClick={() => setValue(false)}
+                    sx={(!value) ? ({...sharedBtnStyle, ...selectedBtnStyle}) : ({...sharedBtnStyle, ...notSelectedBtnStyle})}
+            >Nie</Button>
+        </Box>
+    </Box>)
 }
--- frontend/src/pages/auth/sign-up/index.js
+++ frontend/src/pages/auth/sign-up/index.js
@@ -1,11 +1,12 @@
 import AreYouFromClinicForm from "@/components/signup/AreYouFromClinicForm";
 import "@/styles/pages/auth/sign-up.module.scss";
-import {Box, Button, Typography, useTheme} from "@mui/material";
-import {useState} from "react";
+import {Box, Button, Stack, Typography, useTheme} from "@mui/material";
+import {useEffect, useState} from "react";
 import ClinicCodeForm from "@/components/signup/ClinicCodeForm";
 import IsItYourProblemForm from "@/components/signup/IsItYourProblemForm";
 import WhatAreYourAddictionForm from "@/components/signup/WhatAreYourAddictionForm";
 import GenerateYourNicknameForm from "@/components/signup/GenerateYourNicknameForm";
+import Image from "next/image";
 
 export default function SignUp() {
     const [currentStep, setCurrentStep] = useState(0);
@@ -15,54 +16,79 @@ export default function SignUp() {
     const [clinicCode, setClinicCode] = useState("");
     const onNext = () => {
         setCurrentStep(step => step + 1);
-        console.log(isFromClinic, currentStep)
     }
     const onPrev = () => {
         setCurrentStep(step => step - 1);
     }
 
     const theme = useTheme();
-
+    useEffect(() => {
+        if (document?.body) {
+            document.body.style.backgroundColor = theme.palette.primary.light;
+        }
+    }, [theme]);
     return (
-        <Box sx={{
-            width: "100vw",
-            height: "100vh",
-            overflowX: "hidden",
-            backgroundColor: theme.palette.primary.light,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
+        <>
+            <Stack sx={{
+                backgroundColor: theme.palette.primary.light,
+                width: "100vw",
+                overflow: "hidden"
+            }}>
+                <Image sx={{marginBottom: 20}} src={"/logo.png"} alt={"logo"} width={50} height={50}/>
+            </Stack>
             <Box sx={{
-                paddingTop: 3,
-                paddingLeft: 5,
-                paddingRight: 5,
-                paddingBottom: 3,
-                borderRadius: 2,
-                width: "100%",
-                maxWidth: "20rem",
-                backgroundColor: "white",
+                width: "100vw",
+                minHeight: "90vh",
+                height: "100%",
+                overflowX: "hidden",
                 display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+
+                alignContent: "center",
                 flexDirection: "column"
             }}>
-                <Typography variant={"h6"}>Opowiedz nam o sobie</Typography>
-                {/*forms body*/}
-                {currentStep === 0 && (
-                    <AreYouFromClinicForm value={isFromClinic} setValue={(v) => setIsFromClinic(v)}/>)}
-                {currentStep === 1 && isFromClinic && (
-                    <ClinicCodeForm value={clinicCode} setValue={(v) => setClinicCode(v)}/>)}
-                {currentStep === 1 && !isFromClinic && (
-                    <IsItYourProblemForm value={isItYourProblem} setValue={(v) => setIsItYourProblem(v)}/>)}
-                {currentStep === 2 && !isFromClinic && (
-                    <WhatAreYourAddictionForm value={addictions} setValue={(v) => setAddictions(v)}/>)}
-                {currentStep === 3 && !isFromClinic && (
-                    <GenerateYourNicknameForm value={addictions} setValue={(v) => setAddictions(v)}/>)}
-                <Box sx={{display: "flex"}}>
-                    <Button sx={{marginTop: 3}} disabled={currentStep === 0} onClick={onPrev}
-                            variant={"outlined"}>Wstecz</Button>
-                    <Button sx={{marginTop: 3, marginLeft: 1}} onClick={onNext} variant={"outlined"}>Dalej</Button>
+                <Box sx={{
+                    paddingTop: 3,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                    paddingBottom: 3,
+                    borderRadius: 2,
+                    width: "100%",
+                    maxWidth: "30rem",
+                    display: "flex",
+                    flexDirection: "column"
+                }}>
+                    {/*forms body*/}
+                    <Box sx={{height: "100%", position: "relative"}}>
+                        {currentStep === 0 && (
+                            <AreYouFromClinicForm value={isFromClinic} setValue={(v) => setIsFromClinic(v)}/>)}
+                        {currentStep === 1 && isFromClinic && (
+                            <ClinicCodeForm value={clinicCode} setValue={(v) => setClinicCode(v)}/>)}
+                        {currentStep === 1 && !isFromClinic && (
+                            <IsItYourProblemForm value={isItYourProblem} setValue={(v) => setIsItYourProblem(v)}/>)}
+                        {currentStep === 2 && !isFromClinic && (
+                            <WhatAreYourAddictionForm value={addictions} setValue={(v) => setAddictions(v)}/>)}
+                        {currentStep === 3 && !isFromClinic && (
+                            <GenerateYourNicknameForm value={addictions} setValue={(v) => setAddictions(v)}/>)}
+                    </Box>
                 </Box>
+                <Stack direction={"column"}>
+                    <Button sx={{
+                        marginTop: 3,
+                        backgroundColor: theme.palette.primary.dark,
+                        width: "12rem",
+                    }} onClick={onNext} variant={"contained"}>Dalej</Button>
+                    <Button sx={{
+                        marginTop: 3,
+                        color: theme.palette.primary.dark,
+                        borderColor: theme.palette.primary.dark,
+                        width: "12rem",
+                    }} disabled={currentStep === 0} onClick={onPrev}
+
+                            variant={"text"}>Wstecz</Button>
+                </Stack>
             </Box>
-        </Box>
+        </>
     )
 }
