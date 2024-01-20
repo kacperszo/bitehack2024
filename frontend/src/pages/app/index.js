b6ef6d301da2ae3faa0c373796c17c50d1b08aa4
Szymon Halski <halskiszymon@gmail.com>
2024-01-20 16:33:37 +0100

    navbar and logo

---
+++ frontend/src/pages/app/index.js
@@ -0,0 +1,39 @@
+import {Box, Grid, Stack, Typography} from "@mui/material";
+import Navbar from "@/components/navbar";
+import Image from "next/image";
+
+export default function AppHome() {
+
+  return (
+    <>
+      <Navbar/>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundImage: 'url(/gradient-bg.svg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      >
+        <Stack sx={{px: 2}}>
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <Image src={"/homecat.png"} alt={"cat"} width={400} height={400}/>
+            </Grid>
+            <Grid item xs={12} md={6} sx={{mt: 3}}>
+              <Typography variant="h3" sx={{mt: 15}}>
+                Witaj w panelu odNowy.
+              </Typography>
+              <Typography variant="body2" sx={{mt: 2}}>
+                Swoje postanowienie trzymasz nieprzerwanie przez 1 dzień.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Stack>
+      </Box>
+    </>
+  );
+}
