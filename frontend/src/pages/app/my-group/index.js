de16cf0f68f416dcfc7a17de4392e4e927941269
FunHB <marcel.krolczyk@gmail.com>
2024-01-21 07:40:48 +0100

    rework mygroup page

--- frontend/src/pages/app/my-group/index.js
+++ frontend/src/pages/app/my-group/index.js
@@ -21,10 +21,13 @@ export default function MyGroupPage({group}) {
                     height: '100%'
                 }}
             >
-                <Stack sx={{mt: 4, px: 5, height: '100%'}} textAlign="left">
+                <Stack sx={{mt: 4, px: 3, height: '100%'}} textAlign="left">
                     <Stack direction="row">
                         <Box flexGrow={1} sx={{
-                            textAlign: 'center'
+                            textAlign: 'center',
+                            backgroundColor: {xs: "#EFFDF3", lg: "transparent"},
+                            borderRadius: 2,
+                            p: 1
                         }}>
                             <Typography fontWeight={600} variant={"h4"} sx={{
                                 display: 'flex',
@@ -34,44 +37,38 @@ export default function MyGroupPage({group}) {
                                     style={{paddingLeft: 10, paddingRight: 10,}}
                                     src={"/icons/Book.svg"} width={30} height={30}
                                     alt={"Book"}/>
-                                Moja grupa wzajemnego wsparcia
-                            </Typography>
-                            <Typography variant="body2">
-                                Porozmawiaj z osobami, które mogą zmagać się z tym samym problemem.
-                                <br/>
-                                System automatycznie przypisuje do Twojej grupy osoby, które zmagają się z tym samym
-                                uzależnieniem.
-                                <br/>
-                                W czacie obecny jest nasz specjalista, który na bieżąco monitoruje waszą sytuację.
+                                Moja grupa wsparcia
                             </Typography>
                         </Box>
                     </Stack>
-                    <Card sx={{pb: 4, height: '100%', boxShadow: 'none'}}>
-                        <CardContent sx={{height: '100%', display: 'flex', flexDirection: 'column'}}>
-                            <Box sx={{
-                                display: 'flex',
-                                gap: 2,
-                                alignItems: 'flex-start',
-                                justifyContent: 'center',
-                                paddingTop: 1,
-                                paddingBottom: 1,
-                            }}>
-                                {users.map((user, index) => {
-                                    return (
-                                        <UserTile key={index} user={user}/>
-                                    )
-                                })}
-                            </Box>
-                            <Box sx={{
-                                paddingTop: 2,
-                                paddingBottom: 1,
-                                flex: '1',
-                                overflow: 'auto'
-                            }}>
-                                <Chat/>
-                            </Box>
-                        </CardContent>
-                    </Card>
+                    <Box sx={{ pt: 5 }}>
+                        <Typography sx={{ color: '#2B5B30', fontWeight: 'bold', py: 1 }}>Uczestnicy grupy</Typography>
+                        <Box sx={{
+                            display: 'flex',
+                            gap: 2,
+                            alignItems: 'flex-start',
+                            justifyContent: { xs: 'flex-start', lg: 'center' },
+                            padding: 2,
+                            paddingBottom: 1,
+                            backgroundColor: {xs: "#EFFDF3", lg: "#D9EDDF"},
+                            borderRadius: 2,
+                        }} className={'without-x-scrollbar'}>
+                            {users.map((user, index) => {
+                                return (
+                                    <UserTile key={index} user={user}/>
+                                )
+                            })}
+                        </Box>
+                        <Box sx={{
+                            paddingTop: 2,
+                            paddingBottom: 1,
+                            flex: '1',
+                            overflow: 'auto'
+                        }}>
+                            <Typography sx={{ fontWeight: 'bold', color: '#2B5B30', mb: 1, mt: 3 }}>Chat</Typography>
+                            <Chat/>
+                        </Box>
+                    </Box>
                 </Stack>
             </Box>
         </Layout>
@@ -82,16 +79,16 @@ export const getServerSideProps = (async () => {
     const group = {
         name: 'Group 1',
         users: [
-            {id: 1, email: 'user1@example.com', displayName: 'Sprytny Wilk', type: 'addict', color: 'red'},
-            {id: 2, email: 'user2@example.com', displayName: 'Dzielny Dzik', type: 'addict', color: 'green'},
-            {id: 3, email: 'user3@example.com', displayName: 'Mądra Zebra', type: 'addict', color: 'blue'},
-            {id: 4, email: 'user4@example.com', displayName: 'Ostrożny Kot', type: 'addict', color: 'orange'},
+            {id: 1, email: 'user1@example.com', displayName: 'Sprytny Wilk', type: 'addict', color: '#264653'},
+            {id: 2, email: 'user2@example.com', displayName: 'Dzielny Dzik', type: 'addict', color: '#2A9D8F'},
+            {id: 3, email: 'user3@example.com', displayName: 'Mądra Zebra', type: 'addict', color: '#E9C46A'},
+            {id: 4, email: 'user4@example.com', displayName: 'Ostrożny Kot', type: 'addict', color: '#F4A261'},
             {
                 id: 5,
                 email: 'psychiatrist@example.com',
-                displayName: 'Jan Kowalski | Poradnia.pl',
+                displayName: 'Jan Kowalski',
                 type: 'psychiatrist',
-                color: 'violet'
+                color: '#E76F51'
             }
         ]
     }
